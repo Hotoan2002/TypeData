467be1e05e3ae169f26fc9c79a35a56ee055583b
Hotoan2002 <toanzzz2k2@gmail.com>
2021-10-20 16:29:50 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,54 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          /**
+           * Thông tin một sinh viên
+           */
+        
+         String tensv ="Ho Toan";                                                                                               
+          System.out.println("Ten sinh vien:" + tensv);  
+         String msv ="2160128";
+         System.out.println("Ma sinh vien:" +msv);
+         String cmnd ="192072148";
+         System.out.println("Chung minh nhan dan:" +cmnd);
+         String tenlop ="21CDTH41";
+         System.out.println("Ten lop:" +tenlop);
+         String tentruong ="cdcnh";
+         double dtb = 8f;
+         System.out.println("Diem trung binh:" +dtb);
+         
+         /** Thông tin công nhân
+          * 
+          */
+         String tencn ="Toan";
+         System.out.println("Ten Cong nhan:" +tencn);
+         String diachi ="hue";
+         System.out.println("Dia chi :" +diachi);
+         String sdt ="0901134869";
+         System.out.println("So Dien Thoai :" +sdt);
+         String luong ="9000000";
+         System.out.println("He so luong :" +luong);
+         
+         /** Thông tin một chiếc xe
+          * 
+          */
+         String tenxe ="BMW 320i Sport Line Plus";
+         System.out.println("Ten xe:" +tenxe);
+         String hangxe ="BMW";
+         System.out.println("hang xe :" +hangxe);
+         String giaxe ="9ty";
+         System.out.println("Gia xe :" +giaxe);
+         String mauson ="red";
+         System.out.println("mau son :" +mauson);
+         
+        		 
+         
+        		 
+        		                                     
+                                   		                  		         		 
+	}
+
+}
